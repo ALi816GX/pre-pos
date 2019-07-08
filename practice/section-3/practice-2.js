c03be55d83e4b9f84c74c82fc5511593eb6ad632
ALi816GX <854501715@qq.com>
2019-07-08 20:05:21 +0800

    Update practice-2.js

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  const values = objectB["value"];
+  for(let i = 0;i< values.length;i++){
+    for(let j = 0;j < collectionA.length;j++){
+     if(values[i] == collectionA[j]["key"]){
+
+        var temp =(collectionA[j]["count"] - 3)/3;
+        temp = parseInt(temp);
+        collectionA[j]["count"]--;
+        collectionA[j]["count"] = collectionA[j]["count"] - temp;
+        continue;
+       
+      }
+    }
+  }
+
+  console.log(collectionA);
+
+  return collectionA;
 }
